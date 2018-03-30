dc50405776d00e8ba7ddd091d53386633155dbaf
Analogix <1553392750@qq.com>
2018-03-30 14:33:41 +0800

    Update power-on-and-down.c
    Added initial settings in power_on function.

--- power-on-and-down.c
+++ power-on-and-down.c
@@ -7,7 +7,41 @@
 #include "Anx74XXLib.h"
 #include "Anx74XXLibReg.h"
 
-
+//User needs to update these values according the device system
+//--begin
+UINT32 g_source_capabilities[1] = {
+//5V,1.5A,Dual-role power, USB Communicateions Capable,
+//data role swap, untruncked support(PD3.0)
+   PDO_FIXED(PD_VOLTAGE_5V, PD_CURRENT_1500MA,
+   (PDO_TYPE_FIXED | PDO_FIXED_DUAL_ROLE | PDO_FIXED_COMM_CAP |\
+   PDO_FIXED_DATA_SWAP | PDO_FIXED_UNCHUNKED_SUPPORTED))
+};
+UINT32 g_sink_capabilities[4] = {
+   /*5V, 3A, Fixed */
+   PDO_FIXED(PD_VOLTAGE_5V, PD_CURRENT_3A,
+   (PDO_TYPE_FIXED | PDO_FIXED_DUAL_ROLE | PDO_FIXED_COMM_CAP |\
+   PDO_FIXED_DATA_SWAP | PDO_FIXED_SUSPEND_HIGHER)),
+   /*12V, 3A */
+   PDO_FIXED(PD_VOLTAGE_12V, PD_CURRENT_3A,
+   	(PDO_TYPE_FIXED | PDO_FIXED_DUAL_ROLE | PDO_FIXED_COMM_CAP |\
+   	PDO_FIXED_DATA_SWAP)),
+   /*20v,3A */
+   PDO_FIXED(PD_VOLTAGE_20V, PD_CURRENT_3A,
+   	(PDO_TYPE_FIXED | PDO_FIXED_DUAL_ROLE | PDO_FIXED_COMM_CAP | \
+   	PDO_FIXED_DATA_SWAP)),
+   PDO_VAR(PD_VOLTAGE_5V, PD_VOLTAGE_20V, PD_CURRENT_3A)
+};
+UINT8 g_SVID[4] = { 0x00, 0x00, 0x00, 0xff };
+//VDO in Discovery Modes response
+//DFP_D-capable
+UINT8 g_DP_Capabilities[4] = { 0xC6, 0x00, 0x00, 0x00 };
+//Identity response
+UINT8 g_Indentity[12] = {
+	0x00, 0x00, 0x00, 0x90, //0x90,	snk_id_hdr, only host; 0xD0, host/device, PDUSB peripheral */
+	0x00, 0x00, 0x00, 0x00,	/*snk_cert*/
+	0x00, 0x00, 0x00, 0x00 /*snk_prd*/
+};
+//--end
 
 VOID efuse_read(UINT8 slave_addr, UINT8 offset, UINT8 len, UINT8 *buf)
 {
@@ -35,8 +69,7 @@ UINT8 get_chip_rev(UINT8 slave_addr)
 VOID power_on(UINT8 spi_slave_addr, UIN8 tcpc_slave_addr)
 {
 	UINT8 val = 0;
-	INT8 count = 100;
-	
+	INT8 count = 200;
     //Make sure OCM is running
 	while (val == 0)
 	{
@@ -78,8 +111,11 @@ VOID power_on(UINT8 spi_slave_addr, UIN8 tcpc_slave_addr)
 	//configure RDO, these parameters  need to be set addroding the device's power capability.
 	anx74xx_set_power_negotiation_parameter(spi_slave_addr, PD_MAX_VOLTAGE_20V, PD_POWER_60W, PD_POWER_15W);
 	//Send initial settings
-	//anx74xx_set_source_cap(...);
-	//anx74xx_set_sink_cap(...);
+	anx74xx_set_source_cap(spi_slave_addr, g_source_capabilities, 1);
+	anx74xx_set_sink_cap(spi_slave_addr, g_sink_capabilities, 4);
+	anx74xx_set_initial_msg_info(spi_slave_addr, g_Indentity, 12, TYPE_DP_SNK_IDENTITY);
+	anx74xx_set_initial_msg_info(spi_slave_addr, g_SVID, 4, TYPE_SVID);
+	anx74xx_set_initial_msg_info(spi_slave_addr, g_DP_Capabilities, 4, TYPE_DP_CAP);
 }
 
 VOID power_down(UINT8 spi_slave_addr, UIN8 tcpc_slave_addr)
